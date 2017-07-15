ef84629605049261e9be9ee0e18013db24d7ddff
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-07-15 14:33:26 +0900

    todos is

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -33,6 +36,25 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.send(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    //params key value pair with url and id
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+        
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
