7b17a6737bb5373c1f27e16d8d17071a193e7d01
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-07-16 23:42:50 +0900

    almost done

--- server/server.js
+++ server/server.js
@@ -134,7 +134,20 @@ app.post('/users', (req, res) => {
 });
 
 app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);
+    res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+//    res.send(body);
 });
 
 app.listen(port, () => {
