1646192b17bc354d49e8b31de8c03821af41bb7c
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-07-17 17:12:05 +0900

    test logout

--- server/models/user.js
+++ server/models/user.js
@@ -127,7 +127,7 @@ UserSchema.pre('save', function(next){
     if(user.isModified('password')){
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
-               user.password = hash; 
+               user.password = hash;
                 next();
            });
         });
